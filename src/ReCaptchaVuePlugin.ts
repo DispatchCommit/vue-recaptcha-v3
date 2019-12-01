dda4b3317c307a68c3ba7c437acf99b092c33d80
Dispatch <45262335+DispatchCommit@users.noreply.github.com>
2019-12-01 10:04:07 -0800

    reject promise on error

--- src/ReCaptchaVuePlugin.ts
+++ src/ReCaptchaVuePlugin.ts
@@ -5,16 +5,19 @@ import { IReCaptchaOptions } from './IReCaptchaOptions'
 export function VueReCaptcha (Vue: typeof _Vue, options: IReCaptchaOptions): void {
   const plugin = new ReCaptchaVuePlugin()
   let recaptchaLoaded = false
+  let recaptchaError = false
 
   const loadedWaiters: Array<(resolve: boolean) => void> = []
 
   // eslint-disable-next-line @typescript-eslint/promise-function-async
   Vue.prototype.$recaptchaLoaded = () => new Promise<boolean>((resolve, reject) => {
-    if (recaptchaLoaded === true) {
-      resolve(true)
-    } else {
-      loadedWaiters.push(resolve)
+    if (recaptchaError === false) {
+      return reject('reCAPTCHA Error');
     }
+    if (recaptchaLoaded === true) {
+      return resolve(true)
+    }
+    loadedWaiters.push((success) => success ? resolve(true) : reject('reCAPTCHA Error'))
   })
 
   plugin.initializeReCaptcha(options).then((wrapper) => {
@@ -25,7 +28,10 @@ export function VueReCaptcha (Vue: typeof _Vue, options: IReCaptchaOptions): voi
 
     Vue.prototype.$recaptchaInstance = wrapper
     loadedWaiters.forEach((v) => v(true))
-  }).catch(console.error)
+  }).catch((error) => {
+    console.error(error)
+    loadedWaiters.forEach((v) => v(false))
+  })
 }
 
 class ReCaptchaVuePlugin {
