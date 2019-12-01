9f869cdbe5c3bdc3abbf52b6d7c7a5e8afae28c5
DispatchCommit <dispatchcommit@gmail.com>
2019-12-01 11:46:42 -0800

    Update typescript stuff

--- src/ReCaptchaVuePlugin.ts
+++ src/ReCaptchaVuePlugin.ts
@@ -5,13 +5,13 @@ import { IReCaptchaOptions } from './IReCaptchaOptions'
 export function VueReCaptcha (Vue: typeof _Vue, options: IReCaptchaOptions): void {
   const plugin = new ReCaptchaVuePlugin()
   let recaptchaLoaded = false
-  let recaptchaError = null
+  let recaptchaError: Error = null
 
-  const loadedWaiters: Array<(resolve: boolean) => void> = []
+  const loadedWaiters: Array<({resolve: (resolve?: boolean) => void, reject: (reject?: Error) => void})> = []
 
   // eslint-disable-next-line @typescript-eslint/promise-function-async
   Vue.prototype.$recaptchaLoaded = () => new Promise<boolean>((resolve, reject) => {
-    if (recaptchaError === true) {
+    if (recaptchaError !== null) {
       return reject(recaptchaError)
     }
     if (recaptchaLoaded === true) {
